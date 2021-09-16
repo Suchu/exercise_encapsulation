ec57992aab39f14c51b43cc5bb8d80e17519cb44
Sulochana Bhujel <sulochana.bhujel92@gmail.com>
2021-09-16 17:42:06 +0100

    Changed format and diagram

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -3,16 +3,15 @@ package com.company;
 public class Student {
 
     protected String name;
-    protected char[] studentGrade = { 'A', 'B', 'C', 'D', 'E', 'F' };
-    protected char grade;
+    private char[] studentGrade = {'A', 'B', 'C', 'D', 'E', 'F'};
+    private char grade;
     protected int group;
-    private String secretNickName;
+    private String secretNickName = "MySecretNickName";
 
     public Student(String name, char grade, int group) {
         this.name = name;
         this.grade = grade;
         this.group = group;
-        this.secretNickName = "MySecretNickName";
     }
 
     public String getName() {
@@ -27,36 +26,27 @@ public class Student {
         return group;
     }
 
-    public void setSecretNickName(String myNickName) {
-        this.secretNickName = myNickName;
-    }
-
     public void upgrade() {
         for (int i = 0; i < studentGrade.length; i++) {
-            if (studentGrade[i] == grade && i != studentGrade.length - 1) {
-                grade = studentGrade[i + 1];
-                break;
+            if (studentGrade[i] == grade && i != 0) {
+                grade = studentGrade[i - 1];
             }
         }
-
     }
 
     public void downgrade() {
-        System.out.println("grade: " + grade);
-        for (int i = 0; i <= studentGrade.length; i++) {
-            if (studentGrade[i] == grade && i != 0) {
-                grade = studentGrade[i - 1];
-                break;
-            }
-        }
+//        for (int i = 0; i < studentGrade.length; i++) {
+//            if (studentGrade[i] == grade && i != 0) {
+//                grade = studentGrade[i + 1];
+//            }
+//        }
     }
 
     public static void main(String[] args) {
-        Student student = new Student("Michelle", 'B', 1);
+        Student student = new Student("Michelle", 'F', 1);
         System.out.println("Name: " + student.getName());
         System.out.println("Grade: " + student.getGrade());
         System.out.println("Group: " + student.getGroup());
-        System.out.println("Nickname: " + student.secretNickName);
         student.upgrade();
         System.out.println("Upgraded to: " + student.getGrade());
         student.downgrade();
