7e8a56f7c33ddd5035925bb4c2900afe4b5ccebd
Sulochana Bhujel <sulochana.bhujel92@gmail.com>
2021-09-16 19:25:22 +0100

    Added secret nick name method and changed for loop

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -3,10 +3,10 @@ package com.company;
 public class Student {
 
     protected String name;
-    private char[] studentGrade = {'A', 'B', 'C', 'D', 'E', 'F'};
+    private final char[] studentGrade = {'A', 'B', 'C', 'D', 'E', 'F'};
     private char grade;
     protected int group;
-    private String secretNickName = "MySecretNickName";
+    private String secretNickName;
 
     public Student(String name, char grade, int group) {
         this.name = name;
@@ -26,27 +26,39 @@ public class Student {
         return group;
     }
 
+    public String getSecretNickName() {
+        return secretNickName;
+    }
+
+    public void setSecretNickName(String mySecretNickName) {
+        this.secretNickName = mySecretNickName;
+    }
+
     public void upgrade() {
-        for (int i = 0; i < studentGrade.length; i++) {
-            if (studentGrade[i] == grade && i != 0) {
-                grade = studentGrade[i - 1];
+        for (char c : studentGrade) {
+            if (c == grade && c > 'A') {
+                grade--;
+                return;
             }
         }
     }
 
     public void downgrade() {
-//        for (int i = 0; i < studentGrade.length; i++) {
-//            if (studentGrade[i] == grade && i != 0) {
-//                grade = studentGrade[i + 1];
-//            }
-//        }
+        for (char c : studentGrade) {
+            if (c == grade && c < 'F') {
+                grade++;
+                return;
+            }
+        }
     }
 
     public static void main(String[] args) {
-        Student student = new Student("Michelle", 'F', 1);
+        Student student = new Student("Michelle", 'A', 1);
         System.out.println("Name: " + student.getName());
         System.out.println("Grade: " + student.getGrade());
         System.out.println("Group: " + student.getGroup());
+        student.setSecretNickName("Coco");
+        System.out.println("Nick Name: " + student.getSecretNickName());
         student.upgrade();
         System.out.println("Upgraded to: " + student.getGrade());
         student.downgrade();
