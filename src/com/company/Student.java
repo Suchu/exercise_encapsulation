dbee682c2ae576d5530aa89355218c34796a18ab
Sulochana Bhujel <sulochana.bhujel92@gmail.com>
2021-09-22 17:22:43 +0100

    Reimplemented getGrade method

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -2,27 +2,22 @@ package com.company;
 
 public class Student {
 
-    private final char[] studentGrade = {'A', 'B', 'C', 'D', 'E', 'F'};
     private final String secretNickName = "MySecretNickName";
+    private final char[] grade = {'A', 'B', 'C', 'D', 'E', 'F'};
+    private final char currentGrade;
     protected String name;
     protected int group;
-    private char grade;
 
-    public Student(String name, char grade, int group) {
+    public Student(String name, char currentGrade, int group) throws Exception {
         this.name = name;
-        this.grade = grade;
+        this.currentGrade = Character.toUpperCase(currentGrade);
         this.group = group;
-    }
-
-    public static void main(String[] args) {
-        Student student = new Student("Michelle", 'A', 1);
-        System.out.println("Name: " + student.getName());
-        System.out.println("Grade: " + student.getGrade());
-        System.out.println("Group: " + student.getGroup());
-        student.upgrade();
-        System.out.println("Upgraded to: " + student.getGrade());
-        student.downgrade();
-        System.out.println("Downgraded to: " + student.getGrade());
+        if (!(this.currentGrade <= 'F')) {
+            throw new Exception("Grade must be one of the following: A, B, C, D, E, F");
+        }
+        if (!(this.group >= 1 && this.group <= 5)) {
+            throw new Exception("Group must be one of the following: 1, 2, 3, 4, 5");
+        }
     }
 
     public String getName() {
@@ -30,28 +25,29 @@ public class Student {
     }
 
     public char getGrade() {
-        return grade;
+        for (int i = 0; i < grade.length; i++) {
+            if (grade[i] == currentGrade) {
+                if (currentGrade != grade[0]) {
+                    char upgrade = grade[i - 1];
+                    System.out.println("Upgraded To: " + upgrade);
+                }
+                if (currentGrade != grade[grade.length - 1]) {
+                    char downgrade = grade[i + 1];
+                    System.out.println("Downgraded To: " + downgrade);
+                }
+            }
+        }
+        return currentGrade;
     }
 
     public int getGroup() {
         return group;
     }
 
-    public void upgrade() {
-        for (char c : studentGrade) {
-            if (c == grade && c > 'A') {
-                grade--;
-                return;
-            }
-        }
-    }
-
-    public void downgrade() {
-        for (char c : studentGrade) {
-            if (c == grade && c < 'F') {
-                grade++;
-                return;
-            }
-        }
+    public static void main(String[] args) throws Exception {
+        Student student = new Student("Michelle", 'B', 1);
+        System.out.println("Name: " + student.getName());
+        System.out.println("Current Grade: " + student.getGrade());
+        System.out.println("Group: " + student.getGroup());
     }
 }
